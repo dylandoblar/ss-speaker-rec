cfc9b5eb01f10594253c681d8aa04d2e010449c6
Wonjune Kang <wjkang@mit.edu>
2020-11-02 02:51:50 +0000

    modified pase extraction script to save to npy arrays instead of torch tensors

--- pase/run_pase_voxceleb.py
+++ pase/run_pase_voxceleb.py
@@ -2,6 +2,8 @@ import sys
 import os
 import torch
 import librosa
+import numpy as np
+from tqdm import tqdm
 
 from pase.models.frontend import wf_builder
 
@@ -14,12 +16,15 @@ def get_pase_representations(pase_model, audio_path):
     y, fs = librosa.core.load(audio_path, sr=None)
     y = torch.tensor(y)[(None,)*2].to(device)  # unsqueeze twice at first dim
     pase_reps = pase_model(y)
+    pase_reps = pase_reps.detach().cpu().numpy()
     return pase_reps
 
 if __name__ == "__main__":
     audio_path = sys.argv[1]
     save_path = sys.argv[2]
     
+    utter_per_spkr = 50
+
     # load model
     pase = wf_builder('cfg/frontend/PASE+.cfg').eval()
     pase = pase.to(device)
@@ -32,13 +37,7 @@ if __name__ == "__main__":
             speaker_ids.remove(speaker_id)
 
     # get PASE representations for each speaker
-    for i, speaker_id in enumerate(speaker_ids):
-        if i % 100 == 0:
-            print(f"processing {i}th speaker...")
-
-        # if os.path.exists(os.path.join(save_path, speaker_id)):
-        #     continue
-        
+    for speaker_id in tqdm(speaker_ids):
         os.makedirs(os.path.join(save_path, speaker_id), exist_ok=True)
         path_to_speaker = os.path.join(audio_path, speaker_id)
         
@@ -51,13 +50,13 @@ if __name__ == "__main__":
             path_to_utters = os.path.join(path_to_speaker, video_id)
             utterances = os.listdir(path_to_utters)
             for j, utt in enumerate(utterances):
-                if int(utt[:-4]) == 50:
+                if int(utt[:-4]) <= utters_per_spkr:
                     utter_path = os.path.join(path_to_utters, utt)
                     pase_reps = get_pase_representations(pase, utter_path)
                      
-                    save_filename = utt[:-4]+'.pt'
+                    save_filename = utt[:-4]+'.npy'
                     save_file_path = os.path.join(save_path, speaker_id, save_filename)
-                    torch.save(pase_reps, save_file_path)
+                    np.save(save_file_path, pase_reps)
 
 
 
