6bf95b4cbbbc48d2b30dccaedf7fc533a488cd86
Wonjune Kang <wonjunekang@Wonjunes-Work-MacBook-Pro.local>
2020-11-04 18:41:24 -0500

    modified dataloader for voxceleb formatting

--- ge2e_lstm/data_load.py
+++ ge2e_lstm/data_load.py
@@ -10,39 +10,44 @@ from torch.utils.data import Dataset
 
 
 class VoxCelebDataset(Dataset):
-    def __init__(self, data_path, M, num_frames, utter_start=0, shuffle=True):
-        self.path = data_path
+    def __init__(self, data_path, M, num_frames, shuffle=True):
+        self.data_path = data_path
         self.M = M
         self.num_frames = num_frames
-        self.file_list = os.listdir(self.path)
+        self.file_list = os.listdir(self.data_path)
         self.shuffle = shuffle
-        self.utter_start = utter_start
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, idx):
-        np_file_list = os.listdir(self.path)
+        self.file_list = os.listdir(self.data_path)
 
         if self.shuffle:
             # select random speaker
-            selected_spkr = random.sample(np_file_list, 1)[0]
+            selected_spkr = random.sample(self.file_list, 1)[0]
         else:
-            selected_spkr = np_file_list[idx]
+            selected_spkr = self.file_list[idx]
 
-        # load utterance spectrograms for selected speaker
-        all_utters = np.load(os.path.join(self.path, selected_spkr))
+        # randomly select M utterance files per speaker
+        path_to_spkr_utts = os.path.join(self.data_path, selected_spkr)
+        sampled_utters = random.sample(path_to_spkr_utts, M)
 
-        # select M utterances per speaker
-        # dims are ordered as (batch(=M), n_mels(=40), frames)
-        if self.shuffle:
-            utter_idx = np.random.randint(0, all_utters.shape[0], self.M)
-            sampled_utters = all_utters[utter_idx]
-        else:
-            # utterances of a speaker 
-            sampled_utters = all_utters[self.utter_start:self.utter_start+self.M]
+        # load in the M selected utterances
+        # dims are ordered as (n_feats (n_mels or pase_dim), frames)
+        selected_utters = []
+        for full_utter in sampled_utters:
+            utter_spec = np.load(os.path.join(path_to_spkr_utts, full_utter))
+            start_frame = np.random.randint(0, utter_spec.shape[1]-self.num_frames)
+            partial_utt = utter_spec[:,start_frame:start_frame+self.num_frames]
+            selected_utters.append(partial_utt)
+        selected_utters = np.stack(selected_utters, axis=0)  # (batch, n_feats, frames)
 
-        # transpose [batch, frames, n_mels]
-        sampled_utters = torch.tensor(np.transpose(sampled_utters, axes=(0,2,1)))
-        return sampled_utters
+        # transpose to (batch, frames, n_feats)
+        selected_utters = torch.tensor(np.transpose(selected_utters, axes=(0,2,1)))
+        return selected_utters
+
+
+
+
 
